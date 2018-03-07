1f97bb08751c685c206ab9f741e72039df83e0fe
ShawnshanksGui <fei467475649@gmail.com>
2018-03-07 11:15:51 +0800

    path_selector, bitrate_selector

--- server_MPath_proto/src/path_selector.cpp
+++ server_MPath_proto/src/path_selector.cpp
@@ -22,15 +22,15 @@ void Path_Selector::select_Path(Channel_Inf chan_inf[],
 		}
 	}
 */
-		for(int i = 0; i < REGION_NUM; i++) {
-			if(i < 2) {
-				for(int k = 0; k < video_reader.frame_num; k++) {
-					path_decs[k][i] = CELLULAR_NETWORK; 
-			    }
-			}
-			else {
-				for(int k = 0; k < FRAME_GOP; k++) {
-					path_decs[k][i] = WIFI; 				
-			}
+	for(int i = 0; i < REGION_NUM; i++) {
+		if(i < 2) {
+			for(int k = 0; k < video_reader.frame_num; k++) {
+				path_decs[k][i] = CELLULAR_NETWORK; 
+		    }
 		}
+		else {
+			for(int k = 0; k < FRAME_GOP; k++) {
+				path_decs[k][i] = WIFI; 				
+		}
+	}
 }
